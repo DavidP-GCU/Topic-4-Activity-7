13ce16ee4813c0027aaffcf5c65635ca8e4dc796
DavidP-GCU <114124010+DavidP-GCU@users.noreply.github.com>
2022-10-09 15:22:16 -0600

    Add files via upload

---
+++ Activity 7/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Activity_7
+{
+    partial class activity7
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.numOfTermsLabel = new System.Windows.Forms.Label();
+            this.usersNumTerms = new System.Windows.Forms.TextBox();
+            this.userTotalLabel = new System.Windows.Forms.Label();
+            this.userTotal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.calculateButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.calculateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.calculateButton.Location = new System.Drawing.Point(35, 108);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(118, 53);
+            this.calculateButton.TabIndex = 0;
+            this.calculateButton.Text = "CALCULATE";
+            this.calculateButton.UseVisualStyleBackColor = false;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // numOfTermsLabel
+            // 
+            this.numOfTermsLabel.AutoSize = true;
+            this.numOfTermsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numOfTermsLabel.Location = new System.Drawing.Point(30, 52);
+            this.numOfTermsLabel.Name = "numOfTermsLabel";
+            this.numOfTermsLabel.Size = new System.Drawing.Size(149, 22);
+            this.numOfTermsLabel.TabIndex = 1;
+            this.numOfTermsLabel.Text = "Enter # of Terms:";
+            this.numOfTermsLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // usersNumTerms
+            // 
+            this.usersNumTerms.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usersNumTerms.Location = new System.Drawing.Point(209, 54);
+            this.usersNumTerms.Name = "usersNumTerms";
+            this.usersNumTerms.Size = new System.Drawing.Size(100, 23);
+            this.usersNumTerms.TabIndex = 2;
+            // 
+            // userTotalLabel
+            // 
+            this.userTotalLabel.AutoSize = true;
+            this.userTotalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userTotalLabel.Location = new System.Drawing.Point(32, 203);
+            this.userTotalLabel.Name = "userTotalLabel";
+            this.userTotalLabel.Size = new System.Drawing.Size(0, 20);
+            this.userTotalLabel.TabIndex = 3;
+            // 
+            // userTotal
+            // 
+            this.userTotal.AutoSize = true;
+            this.userTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userTotal.Location = new System.Drawing.Point(32, 234);
+            this.userTotal.Name = "userTotal";
+            this.userTotal.Size = new System.Drawing.Size(0, 20);
+            this.userTotal.TabIndex = 4;
+            this.userTotal.Click += new System.EventHandler(this.userTotal_Click);
+            // 
+            // activity7
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(448, 285);
+            this.Controls.Add(this.userTotal);
+            this.Controls.Add(this.userTotalLabel);
+            this.Controls.Add(this.usersNumTerms);
+            this.Controls.Add(this.numOfTermsLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Name = "activity7";
+            this.Text = "Approximate PI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label numOfTermsLabel;
+        private System.Windows.Forms.TextBox usersNumTerms;
+        private System.Windows.Forms.Label userTotalLabel;
+        private System.Windows.Forms.Label userTotal;
+    }
+}
+
---
+++ Activity 7/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_7
+{
+    public partial class activity7 : Form
+    {
+        public activity7()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            // Accidentially double clicked on this propertie which opened up its event handler. 
+            // If I delete this event handler, it will generate an error with my program.
+            // Which I don't know why it does so. Do you have an answer for me Proffessor? 
+            // I know I can and should start over for cleaner code, but I want to know why this is happening. 
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            //Create Variables
+            int num;
+            double piValue = 4;
+            double number = 4;
+            double den = 3;
+            num = Convert.ToInt32(usersNumTerms.Text);
+
+           //Create for Loop
+           for (int i =0; i<=num; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    piValue -= number / den;
+                }
+                else
+                {
+                    piValue += number / den;
+                }
+                den += 2;
+            }
+            //Display Results on Textbox Labels
+            userTotalLabel.Text = "Approximate value of pi after " + num + " terms";
+            userTotal.Text = "= " + piValue;
+        }
+
+        private void userTotal_Click(object sender, EventArgs e)
+        {
+            // Accidentially double clicked on this propertie which opened up its event handler. 
+            // If I delete this event handler, it will generate an error with my program.
+            // Which I don't know why it does so. Do you have an answer for me Proffessor? 
+            // I know I can and should start over for cleaner code, but I want to know why this is happening. 
+        }
+    }
+}
